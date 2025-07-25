ce0d7b3dcaa851a99abcdfcc5feb4feb5fd27186
pangiiz-qq <phebe.phiphi@gmail.com>
2025-07-25 14:33:10 +0700

    submit assignment

--- ex-1.js
+++ ex-1.js
@@ -3,15 +3,21 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary + 5000;
 }
 
 function forEach(array, operation) {
   // Start coding here
+  let result = [];
+  for (let i = 0; i < employeeSalaries.length; i++) {
+    result.push(operation(array[i]));
+  }
+  return result;
 }
 
 // Using `forEach` function here
 
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
